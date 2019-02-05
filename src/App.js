34f1eb106764aad40adfee7b3f9bcec4b09d2e4d
user <user@users-MBP.fritz.box>
2019-02-05 20:26:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { recipes } from "./tempList";
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
 
 class App extends Component {
+  state = {
+    recipes: recipes,
+    url:
+      "https://www.food2fork.com/api/search?key=674afcba83148cd59b3aed618ff5d9ee",
+    base_url:
+      "https://www.food2fork.com/api/search?key=674afcba83148cd59b3aed618ff5d9ee",
+    details_id: 35385,
+    pageIndex: 1,
+    search: "",
+    query: "&q=",
+    error: ""
+  };
+
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+
+      //console.log(jsonData);
+      if (jsonData.recipes.length === 0) {
+        this.setState(() => {
+          return { error: "Sorry, but your search did not match any results" };
+        });
+      } else {
+        this.setState(() => {
+          return { recipes: jsonData.recipes };
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  componentDidMount() {
+    this.getRecipes();
+  }
+
+  displayPage = index => {
+    switch (index) {
+      case 1:
+        return (
+          <RecipeList
+            recipes={this.state.recipes}
+            handleDetails={this.handleDetails}
+            value={this.state.search}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            error={this.state.error}
+          />
+        );
+      case 0:
+        return (
+          <RecipeDetails
+            id={this.state.details_id}
+            handleIndex={this.handleIndex}
+          />
+        );
+      default:
+        break;
+    }
+  };
+
+  handleIndex = index => {
+    this.setState({
+      pageIndex: index
+    });
+  };
+
+  handleDetails = (index, id) => {
+    this.setState({
+      pageIndex: index,
+      details_id: id
+    });
+  };
+
+  handleChange = e => {
+    //console.log("hallo from handle Change");
+    this.setState(
+      {
+        search: e.target.value
+      },
+      () => {
+        //console.log(this.state.search);
+      }
+    );
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    //console.log("from handle submit");
+    const { base_url, search, query } = this.state;
+    this.setState(
+      () => {
+        return {
+          //change a value for a url
+          url: `${base_url}${query}${search}`,
+          search: ""
+        };
+      },
+      () => {
+        this.getRecipes();
+      }
+    );
+  };
+
   render() {
+    // console.log(this.state.recipes);
+    // console.log(this.state.details_id);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>{this.displayPage(this.state.pageIndex)}</React.Fragment>
     );
   }
 }
